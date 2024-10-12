4e9beccd14d8ad26fb49efc08c9c40c0512840eb
minimini1212 <zjonyv12@naver.com>
2024-10-12 09:13:17 +0900

    20241011 알고리즘 문제풀이
    - 다른 사람 풀이 추가

--- 20241011.js
+++ 20241011.js
@@ -62,3 +62,13 @@ solution("EIO"); // 1189
 // b = b.sort((x, y) => x.localeCompare(y));
 
 // console.log(b);
+
+// 다른 사람 풀이
+function solutionOther(words) {
+    return words
+        .split('')
+        .reduce(
+            (r, c, i) => r + [781, 156, 31, 6, 1][i] * ['A', 'E', 'I', 'O', 'U'].indexOf(c) + 1,
+            0,
+        );
+}
